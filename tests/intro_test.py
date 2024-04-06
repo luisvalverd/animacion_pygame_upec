73ae3d942c421d4ee0b271cc3d9d756899f1531a
luis valverde <lvalverde4199@uta.edu.ec>
2024-04-06 05:21:55 +0000

    modificacion de un test

--- tests/intro_test.py
+++ tests/intro_test.py
@@ -1,28 +1,18 @@
 from unittest import TestCase
+from assertpy import assert_that
 
 # modules de la introduccion
+from models.validators.validation import ValidationError
 from controller.intro_controller import IntroAnimation
-from animations.intro import Intro
+from domain.intro_rule import createIntroSprite
 from domain.config import *
 
 class TestIntroAnimation(TestCase):
-  def setUp(self):
-    self.IntroAnimation = IntroAnimation(self.addClassCleanup, self.addClassCleanup)
+  def test_get_exception_create_Intro_with_file_incorret(self):
+    intro = createIntroSprite("test.png", 0, 0)
+    assert_that(intro).is_type_of(ValidationError)
   
-  def test_intro_handler_waiting(self):
-    self.assertEqual(self.IntroAnimation.waitingAnimation(), 0)
+  def test_get_position_image(self):
+    intro = createIntroSprite(LOGO_UPEC_LEFT, 0, 0)
+    assert_that(intro.image.get_rect().topleft).is_equal_to((0, 0))
 
-class TestIntro(TestCase):
-  def setUp(self):
-    self.intro = Intro(LOGO_UPEC_LEFT, 0, 0)
-
-  def test_get_image(self):
-    self.assertEqual(self.intro.image.get_rect().topleft, (0, 0), "Error al obtener las coordenadas superiores") 
-
-  def tearDown(self):
-    self.assertRaises(Intro("test.jpg", 0, 0))
-
-  def test_transition_done(self):
-    self.assertIsNotNone(self.intro.image, "Error no se encontro la imagen")
-
-  
