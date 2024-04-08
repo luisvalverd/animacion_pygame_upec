c993ebd48c8fa7b84ec3a454894d169d8123d346
luis valverde <lvalverde4199@uta.edu.ec>
2024-04-08 07:25:00 +0000

    m

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from controller.sounds_controller import SoundController
 from domain.config import *
 from domain.intro_rule import createAnimation, createIntroSprite
 from domain.sprites_group_rule import createSpritesGroup, addSpriteAGroup
@@ -13,17 +14,20 @@ class Game:
     pygame.init()
     self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     self.clock = pygame.time.Clock()
-    self.font = pygame.font.SysFont("micro", 27)
+    self.font = pygame.font.Font(FONT_BIG_BLUE, 50)
+    self.font.set_bold(True)
     self.running = True
 
     self.background = pygame.image.load("img/center_building.jpeg")
     self.screen.blit(self.background, (0, 0))
 
     # fuegos artificiales
-    self.fireworks = [FireworkRules() for _ in range(4)]
+    self.fireworks = []
 
-    self.text_surface = self.font.render("Feliz Aniversario UPEC", True, (255, 255, 255))
-    self.text_rect = self.text_surface.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))  
+    self.text_index = 0
+    self.rendered_text = "" 
+    self.time_waiting = 5 * 60
+    self.time_show_letter = 100
 
     # sprites de intro
     self.sprites_intro_group = createSpritesGroup()
@@ -33,15 +37,35 @@ class Game:
     # sprites de people
     self.sprite_people_group = createSpritesGroup() 
     self.people_1 = None
+    self.people_2 = None
+    
+    pygame.mixer.init(devicename="Realtek(R) Audio")    
+
+    # sounds
+    self.bg_music = pygame.mixer.Sound(BG_MUSIC)
+
+    self.people_entrance_sound = pygame.mixer.Sound(PEOPLE_ENTRANCE) 
+    self.mixer_people = SoundController(1, self.people_entrance_sound, 10, 5)
+    self.mixer_people.set_volume(0.2)
+
+    self.people_celebraing_sound = pygame.mixer.Sound(PEOPLE_CELEBRATING)
+    self.mixer_people_celebrating = SoundController(2, self.people_celebraing_sound, 15, 16)
+    self.mixer_people.set_volume(0.2)
+
+    # channel de musica de fondo
+    pygame.mixer.Channel(0).set_volume(0.01)
+    pygame.mixer.Channel(0).play(self.bg_music, -1)
 
     #handlers
     self.handleSpriteIntro()
-    self.handler_animation_intro = createAnimation(self.logo_intro_1, self.logo_intro_2, 2)
-    self.handler_fireworks = createFireworkAnimation(self.fireworks, 7, self.screen)
+    self.handler_animation_intro = createAnimation(self.logo_intro_1, self.logo_intro_2, 4)
+    self.handler_fireworks = createFireworkAnimation(self.fireworks, 15, self.screen)
     self.handleSpritePoeple()
-    self.handler_people = createAnimationPeopleEntrance(self.people_1, 4)
-    
+    self.handler_people = createAnimationPeopleEntrance(self.people_1, self.people_2, 5)
 
+    self.is_bg_img = True
+    self.bg_time = 31 * 60
+  
   """
   maneja la construccion de los sprites 
   agrega los sprites de la intro a un grupo de sprites
@@ -53,8 +77,10 @@ class Game:
     addSpriteAGroup(self.sprites_intro_group, self.logo_intro_2)
 
   def handleSpritePoeple(self):
-    self.people_1 = createPeopleSprite(PEOPLE_1, 0, WIN_HEIGHT)
+    self.people_1 = createPeopleSprite(PEOPLE_1, 0, WIN_HEIGHT - 20)
+    self.people_2 = createPeopleSprite(PEOPLE_2, 0, WIN_HEIGHT)
     addSpriteAGroup(self.sprite_people_group, self.people_1)
+    addSpriteAGroup(self.sprite_people_group, self.people_2)
 
   def start(self):
     while self.running:
@@ -64,34 +90,70 @@ class Game:
           self.running = False
 
         # generar fuegos artificiales 
+        
         if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_1:
             self.fireworks.append(FireworkRules()) 
 
-          if event.key == pygame.K_2:
-            for _ in range(7):
-              self.fireworks.append(FireworkRules())
-
-      if random.randint(0, 20) == 1:
-        self.fireworks.append(FireworkRules())
+      
+      if self.handler_fireworks.waiting_time <= 0:
+        if random.randint(0, 20) == 1:
+          self.fireworks.append(FireworkRules())
 
       self.handler_fireworks.waitingAnimation()      
 
-      self.screen.blit(self.background, (0, 0))
-      self.screen.blit(self.text_surface, self.text_rect)
+      if self.bg_time <= 0:
+        self.is_bg_img = False
+      else:
+        self.bg_time -= 1
+
+      if self.is_bg_img:
+        self.screen.blit(self.background, (0, 0))
+      else:
+        self.screen.fill((0, 0, 0))
+        
 
       # people group
       self.handler_people.waitingAnimation()
       if not self.handler_people.get_waiting_done():
-        self.sprite_people_group.showSprite(self.screen)  
+        self.sprite_people_group.showSprite(self.screen) 
 
+      # people sound
+      self.mixer_people_celebrating.waitingSound()
+      self.mixer_people.waitingSound()
+
+      
+      if self.time_waiting <= 0:
+        if self.time_show_letter == 0:
+          if self.text_index < len(TEXT_UPEC):
+            self.rendered_text += TEXT_UPEC[self.text_index]
+            self.text_index += 1
+        elif self.time_show_letter < 0:
+          self.time_show_letter = 0
+        else: 
+          self.time_show_letter -= 1
+      self.time_waiting -= 1
+
+
+      # Renderizar texto con contorno
+      outline = self.font.render(self.rendered_text, True, (0, 0, 0))
+      outline_text_rect = outline.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
+      self.screen.blit(outline, outline_text_rect.move(2, 2))
+
+      # Renderizar texto
+      rendered_text_surface = self.font.render(self.rendered_text, True, (56, 176, 0))
+      text_rect = rendered_text_surface.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
+      self.screen.blit(rendered_text_surface, text_rect)
+      
       # maneja todo de la animacion
       self.handler_animation_intro.waitingAnimation()      
       if not self.handler_animation_intro.get_waiting_done():
         self.sprites_intro_group.showSprite(self.screen)
 
-      pygame.display.flip()
+        
 
+      pygame.display.flip()
+    pygame.mixer.Channel(0).stop()
     pygame.quit()
 
 if __name__ == "__main__":
